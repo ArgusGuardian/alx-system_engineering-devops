cb02326b623110e6e3c513f799032d5ae6920984
Alaa Eddine <Alaaelhannaoui@proton.me>
2024-01-03 18:38:45 +0100

    last done

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 from_match = ARGV[0].scan(/from:([^\]]+)/).flatten.first
-to_match = ARGV[0].scan(/to:(\+\d+)/).flatten.first
+to_match = ARGV[0].scan(/to:([^\]]+)/).flatten.first
 flags_match = ARGV[0].scan(/flags:([\d:-]+)/).flatten.first
+
 puts "#{from_match},#{to_match},#{flags_match}"
